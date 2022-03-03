2257a297fdbe26cd1efaa590079a651028cc66fb
JesusRamirezLpz <jesus.ramirez.lpz@gmail.com>
2022-03-02 19:33:29 -0600

    Se termina el disenio de la interfaz grafica

--- app/src/main/java/com/example/myweatherapp/views/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp/views/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.myweatherapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import com.example.myweatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.update -> {
+                Toast.makeText(this,"Menú seleccionado", Toast.LENGTH_SHORT).show()
+                //showCreateUserDialog("27")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
